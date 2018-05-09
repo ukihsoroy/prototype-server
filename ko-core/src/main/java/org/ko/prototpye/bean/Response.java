8dff52fed1c5cbb5cea097ff75a53180e1fe3f2e
artisters <you_leet@foxmail.com>
2018-05-09 18:08:34 +0800

    #ko-boot

---
+++ ko-core/src/main/java/org/ko/prototpye/bean/Response.java
@@ -0,0 +1,14 @@
+package org.ko.prototpye.bean;
+
+public class Response<T> {
+
+    private String code;
+
+    private String message;
+
+    private boolean success;
+
+    private T data;
+
+
+}
