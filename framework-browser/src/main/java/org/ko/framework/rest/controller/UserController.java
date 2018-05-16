39df68dbf39d38150aa0ea996ce45d5e903cbcba
uleetu <you_leet@foxmail.com>
2018-05-16 21:50:03 +0800

    #boot

--- framework-browser/src/main/java/org/ko/framework/rest/controller/UserController.java
+++ framework-browser/src/main/java/org/ko/framework/rest/controller/UserController.java
@@ -1,32 +1,123 @@
 package org.ko.framework.rest.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.ko.framework.core.support.Response;
-import org.ko.framework.rest.condition.UserQueryCondition;
-import org.ko.framework.rest.dto.UserDTO;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.ko.web.dto.User;
+import org.ko.web.dto.UserQueryCondition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.social.connect.web.ProviderSignInUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
-@Validated
-@Api("用户模块")
+@Api("用户服务")
 public class UserController {
 
-    @GetMapping
-    @ApiOperation("获取用户列表")
-    public Response<UserDTO> queryList (UserQueryCondition condition) {
-        return new Response<UserDTO>(new UserDTO());
+    @Autowired private ProviderSignInUtils providerSignInUtils;
+
+    /**
+     *
+     * @param authentication spring mvc 会自动将Authentication用户认证信息注入
+     * @param userDetails 只要和用户有关的信息
+     * @return
+     */
+    @GetMapping("/me")
+    public Object getCurrentUser (Authentication authentication,
+                                  @AuthenticationPrincipal UserDetails userDetails) {
+        //获取用户信息
+//        return SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication.getPrincipal());
+        return userDetails;
     }
 
-    @GetMapping("/{id}")
-    @ApiOperation("通过ID获取用户")
-    public String queryDetail (@PathVariable String id) {
-        return "Hello, world!";
+    @PostMapping("register")
+    public void register (User user, HttpServletRequest request) {
+        System.out.println("注册用户");
+        String userId = user.getUsername();
+        providerSignInUtils.doPostSignUp(userId, new ServletWebRequest(request));
+    }
+
+    @GetMapping
+    @JsonView(User.UserSimpleView.class)
+    @ApiOperation("用户查询列表")
+    public List<User> query(UserQueryCondition condition,
+                            @PageableDefault(page = 1, size = 15, sort = "username,asc") Pageable pageable) {
+        System.out.println(ReflectionToStringBuilder.toString(condition, ToStringStyle.MULTI_LINE_STYLE));
+        System.out.println(pageable.getPageSize());
+        System.out.println(pageable.getPageNumber());
+        System.out.println(pageable.getSort());
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        return users;
+    }
+
+    @GetMapping("{id:\\d+}")
+    @JsonView(User.UserDetailView.class)
+    @ApiOperation("用户详细查询")
+    public User getInfo (@ApiParam("用户ID") @PathVariable String id) {
+//        throw new UserNotExistException("1");
+        System.out.println("getInfo---");
+        User user = new User();
+        user.setUsername("K.O");
+        return user;
+    }
+
+    @PostMapping
+    public User create (@Valid @RequestBody User user/*, BindingResult errors*/) {
+
+//        if (errors.hasErrors()) {
+//            errors.getAllErrors().forEach(System.out::println);
+//        }
+
+        System.out.println(user.getId());
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        System.out.println(user.getBirthday());
+        user.setId("1");
+        return user;
+    }
+
+    @PutMapping("{id:\\d+}")
+    public User update (@Valid @RequestBody User user, BindingResult errors) {
+
+        if (errors.hasErrors()) {
+            errors.getAllErrors().forEach(e -> {
+                FieldError error = FieldError.class.cast(e);
+                String message = error.getField() + ": " + e.getDefaultMessage();
+                System.out.println(message);
+            });
+        }
+
+        System.out.println(user.getId());
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
+        System.out.println(user.getBirthday());
+        user.setId("1");
+        return user;
+    }
+
+    @DeleteMapping("{id:\\d+}")
+    public void delete (@PathVariable String id) {
+        System.out.println(id);
     }
 
 }
---
+++ framework-browser/src/test/java/org/ko/framework/rest/controller/UserControllerTests.java
@@ -0,0 +1,104 @@
+package org.ko.framework.rest.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+//用SpringRunner来运行测试用例
+@RunWith(SpringRunner.class)
+public class UserControllerTests {
+
+    @Autowired private WebApplicationContext context;
+
+    private MockMvc mock;
+
+    @Before
+    public void setup () {
+        mock = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void whenQuerySuccess () throws Exception {
+        String result = mock.perform(get("/user")
+                .param("username", "K.O")
+                .param("age", "18")
+                .param("ageTo", "20")
+//                .param("size", "15")
+//                .param("page", "3")
+//                .param("sort", "age,desc")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(3))
+                .andReturn().getResponse().getContentAsString();
+        System.out.println(result);
+    }
+
+    @Test
+    public void whenGetInfoSuccess () throws Exception {
+        String result = mock.perform(get("/user/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("K.O"))
+                .andReturn().getResponse().getContentAsString();
+        System.out.println(result);
+    }
+
+    @Test
+    public void whenGetInfoFail () throws Exception {
+        mock.perform(get("/user/a")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    public void whenCreateSuccess () throws Exception {
+        String content = "{\"username\":\"K.O\",\"password\":null,\"birthday\":" + new Date().getTime() + "}";
+        System.out.println(new Date().getTime());
+        String result = mock.perform(post("/user")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(content))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andReturn().getResponse().getContentAsString();
+        System.out.println(result);
+    }
+
+    @Test
+    public void whenUpdateSuccess () throws Exception {
+        Date time = new Date(LocalDateTime.now().plusYears(1).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+//        Date time = new Date();
+        String content = "{\"id\":\"1\",\"username\":\"K.O\",\"password\":null,\"birthday\":" + time.getTime() + "}";
+        System.out.println(time.getTime());
+        String result = mock.perform(put("/user/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(content))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andReturn().getResponse().getContentAsString();
+        System.out.println(result);
+    }
+
+    @Test
+    public void whenDeleteSuccess () throws Exception {
+        mock.perform(delete("/user/1").contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
+    }
+}
