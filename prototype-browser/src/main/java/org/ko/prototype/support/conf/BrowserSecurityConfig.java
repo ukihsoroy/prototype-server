a7c9e8ee734e47be0bd004c73aa4f194b837f047
kayoing <ko.shen@hotmail.com>
2018-07-21 11:19:34 +0800

    #prototype-server

--- prototype-browser/src/main/java/org/ko/prototype/support/conf/BrowserSecurityConfig.java
+++ prototype-browser/src/main/java/org/ko/prototype/support/conf/BrowserSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
-@Configuration
+//@Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
