360365914a81a3735c1b522c37383f27f1a6c5cc
kayoing <ko.shen@hotmail.com>
2018-09-08 20:46:21 +0800

    #prototype-browser

--- prototype-browser/src/main/java/org/ko/prototype/rest/system/controller/SystemController.java
+++ prototype-browser/src/main/java/org/ko/prototype/rest/system/controller/SystemController.java
@@ -1,4 +1,21 @@
 package org.ko.prototype.rest.system.controller;
 
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Validated
+@RequestMapping("system")
 public class SystemController {
+
+
+    @GetMapping("menu")
+    public List getMenu () {
+        return null;
+    }
+
 }
