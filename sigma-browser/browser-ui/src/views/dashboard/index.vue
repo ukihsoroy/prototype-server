32ae29c3f1f64a22cecaa94fa278cdaae6093cc0
Jasonccj <981015619@qq.com>
2019-06-05 21:26:34 +0800

    chore: 基础路由

---
+++ sigma-browser/browser-ui/src/views/dashboard/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 class="dashboard">仪表盘页面</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+
+<style>
+.dashboard {
+  background: green;
+}
+</style>
+
