0e0feb3086b2b4da0d0762f807a04633f661b7c1
uzies <ko.shen@hotmail.com>
2019-06-08 15:13:10 +0800

    #调整Mybatis

--- sigma-browser/src/main/java/org/ko/sigma/BrowserApplicationServer.java
+++ sigma-browser/src/main/java/org/ko/sigma/BrowserApplicationServer.java
@@ -1,26 +1,11 @@
 package org.ko.sigma;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@MapperScan(basePackages = "org.ko.sigma.rest.*.repository")
 public class BrowserApplicationServer {
     public static void main(String[] args) {
         SpringApplication.run(BrowserApplicationServer.class, args);
-    }
-
-    /**
-     * Mybatis分页插件
-     * @return
-     */
-    @Bean
-    public PaginationInterceptor paginationInterceptor(){
-        PaginationInterceptor page = new PaginationInterceptor();
-        page.setDialectType("mysql");
-        return page;
     }
 }
---
+++ sigma-browser/src/main/java/org/ko/sigma/conf/MyBatisPlusConfig.java
@@ -0,0 +1,22 @@
+package org.ko.sigma.conf;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(basePackages = "org.ko.sigma.rest.*.repository")
+public class MyBatisPlusConfig {
+
+    /**
+     * Mybatis分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+}
