596cb15efe8759c24fb1e321b83c6225dc3f69b7
Xiao-Cuir <981015619@qq.com>
2019-06-10 00:09:52 +0800

    feat: 新增首页布局，左侧菜单静态构建

---
+++ sigma-browser/browser-ui/src/layout/components/Sidebar/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        mode="vertical"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :active-text-color="variables.menuActiveText"
+      >
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location" />
+            <span>导航一</span>
+          </template>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu" />
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document" />
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting" />
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+import variables from '@/styles/variables.scss'
+
+export default {
+  components: {
+
+  },
+  computed: {
+    variables() {
+      return variables
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
