785f5065db9079d77d416d43f5f816a4fb8e8f45
Xiao-Cuir <981015619@qq.com>
2019-06-11 10:41:08 +0800

    feat: 1.修改打包文件将elementUI以及业务从app中分离 2.修改项目title

--- sigma-browser/browser-ui/vue.config.js
+++ sigma-browser/browser-ui/vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
     }
   },
   configureWebpack: {
-    name: 'vue-element-starter',
+    name: 'sigma-server',
     resolve: {
       alias: {
         '@': resolve('src')
@@ -41,6 +41,44 @@ module.exports = {
     config
       .when(process.env.NODE_ENV === 'development',
         config => config.devtool('cheap-source-map')   //本地环境为source-map方式
+      )
+    config
+      .when(process.env.NODE_ENV !== 'development',
+        config => {
+          config
+            .plugin('ScriptExtHtmlWebpackPlugin')
+            .after('html')
+            .use('script-ext-html-webpack-plugin', [{
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+              inline: /runtime\..*\.js$/
+            }])
+            .end()
+          config
+            .optimization.splitChunks({
+              chunks: 'all',
+              cacheGroups: {
+                libs: {
+                  name: 'chunk-libs',
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: 10,
+                  chunks: 'initial' // only package third parties that are initially dependent
+                },
+                elementUI: {
+                  name: 'chunk-elementUI', // split elementUI into a single package
+                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                },
+                commons: {
+                  name: 'chunk-commons',
+                  test: resolve('src/components'), // can customize your rules
+                  minChunks: 3, //  minimum common number
+                  priority: 5,
+                  reuseExistingChunk: true
+                }
+              }
+            })
+          config.optimization.runtimeChunk('single')
+        }
       )
   }
 }
