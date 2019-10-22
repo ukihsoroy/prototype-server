4f2635d6ac5cbb8b49037891aba3da586cd2fb32
zhiyuan.shen <ko.shen@hotmail.com>
2019-10-22 11:31:14 +0800

    # 添加sms test

---
+++ sigma-core/src/test/java/org/ko/sigma/core/SendSMSTest.java
@@ -0,0 +1,40 @@
+package org.ko.sigma.core;
+
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import org.junit.Test;
+
+public class SendSMSTest {
+
+    @Test
+    public void whenSendMessageSuccess () {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setMethod(MethodType.POST);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        request.putQueryParameter("PhoneNumbers", "");
+        request.putQueryParameter("SignName", "");
+        request.putQueryParameter("TemplateCode", "");
+        request.putQueryParameter("TemplateParam", "{\"code\":\"123456\"}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
