69aa1b1c0cc835ad1f0c761e6fa8d271a331c422
uzies <ko.shen@hotmail.com>
2019-10-23 23:55:18 +0800

    #commit

---
+++ sigma-mysql/src/main/java/org/ko/sigma/data/constants/SmsLogConstants.java
@@ -0,0 +1,35 @@
+package org.ko.sigma.data.constants;
+
+import org.ko.sigma.data.bean.BasicColumnConstants;
+
+public class SmsLogConstants {
+
+    public static class Columns extends BasicColumnConstants {
+
+        /**
+         * 
+         */
+        public static String ID = "id";
+
+        /**
+         * 
+         */
+        public static String USER_ID = "user_id";
+
+        /**
+         * 
+         */
+        public static String TYPE = "type";
+
+        /**
+         * 
+         */
+        public static String INFO = "info";
+
+        /**
+         * 
+         */
+        public static String SMS_CODE = "sms_code";
+    }
+
+}
